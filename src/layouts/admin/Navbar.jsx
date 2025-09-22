2457103fc2459297745ba82561ec2318ec7a805f
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2025-09-22 16:21:37 +0500

    update

--- src/layouts/admin/Navbar.jsx
+++ src/layouts/admin/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = ({ title }) => {
   return (
-    <header className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700'>
-      <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex flex-row items-center'>
-        <Link to="/admin" className='cursor-pointer'><img className='w-[5%] rounded-full' src="/logo.png" alt="" /></Link>
-        <img src="/admin/profile.jpg" className='h-10 w-10 ml-auto rounded-full' alt="my-img" />
+    <header className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex flex-row items-center">
+        <Link to="/admin" className="cursor-pointer">
+          <img className="w-[5%] rounded-full" src="/logo.png" alt="" />
+        </Link>
+        <Link
+          to="/logout"
+          className="bg-charkol text-yellow p-2 py-1 duration-500 cursor-pointer hover:scale-95 rounded-md"
+        >
+          Logout
+        </Link>
       </div>
     </header>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
